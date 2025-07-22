30d9fc35fd2fa704d72882789eb889dd63c21c5b
ayoubaitben <ayouboubay437@gmail.com>
2025-07-22 12:40:59 +0200

    historique de chrono là mais pas parfait

--- rubik-app/src/app/page.js
+++ rubik-app/src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
   const [seconds, setSeconds] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const timerRef = useRef(null);
+  const [history, setHistory] = useState([]);
 
   const formatTime = (totalSeconds) => {
     const hrs = String(Math.floor(totalSeconds / 3600)).padStart(2, "0");
@@ -15,6 +16,13 @@ export default function Home() {
     );
     const secs = String(totalSeconds % 60).padStart(2, "0");
     return `${hrs}:${mins}:${secs}`;
+  };
+
+  const saveCurrentTime = () => {
+    if (seconds > 0) {
+      setHistory((prev) => [...prev, seconds]);
+      setSeconds(0);
+    }
   };
 
   const startStopTimer = () => {
@@ -52,17 +60,29 @@ export default function Home() {
         Site Chrono Rubik's Cube
       </h1>
 
-      <div className="text-center text-8xl p-90 ">{formatTime(seconds)}</div>
-
-      <div className="flex justify-center space-x-6 mt-6">
-        <div className="border-2 text-5xl justify-center flex w-86 bg-green-700 rounded">
+      <div className="text-center text-8xl m-42  ">{formatTime(seconds)}</div>
+      <div className=" w-fit bg-gray-800 p-4 rounded shadow-md ">
+        <h2 className="text-xl text-white mb-2 ">Historique des temps :</h2>
+        <ul className="space-y-2 text-white text-lg ">
+          {history.map((time, index) => (
+            <li key={index}>
+              {index + 1}. {formatTime(time)}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex space-x-6 mt-6 w-fit mx-auto ">
+        <div className="text-5xl justify-center flex w-86 bg-green-700 rounded">
           <button onClick={startStopTimer}>
             {" "}
             {isRunning ? "Stop" : "Démarrer"}
           </button>
         </div>
-        <div className="	bg-red-700 border-2 text-5xl justify-center flex w-86 text-white rounded">
+        <div className="	bg-red-700  text-5xl justify-center flex w-86 text-white rounded ">
           <button onClick={resetTimer}>Reset</button>
+        </div>
+        <div className="bg-blue-700 text-5xl justify-center flex w-86 text-white rounded">
+          <button onClick={saveCurrentTime}>Enregistrer</button>
         </div>
       </div>
     </div>
